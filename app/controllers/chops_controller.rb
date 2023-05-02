e9b46acbd214dfebc5b84239d95ea78ec9a42630
Kimber Ogden <mx.kimber@gmail.com>
2023-05-02 15:12:53 -0500

    add update to chops, change redirect

--- app/controllers/chops_controller.rb
+++ app/controllers/chops_controller.rb
@@ -10,20 +10,28 @@ class ChopsController < ApplicationController
     render: show
   end
 
-  def create
-
+    def create
     @chop = Chop.new(
       problem: params[:chop][:problem],
       solution: params[:chop][:solution],
-      category_id: current_user.id
-    )
-    @pet.save
-    redirect_to "/chops/#{@chop.id}"
+      )
+      if @chop.save
+      redirect_to "/chops/#{@chop.id}" #not sure if correct
+      end
+    end
 
     def edit
       @chop = Chop.find_by(id: params[:id])
       render :edit
     end
 
-
+    def update
+      @chop = Chop.find_by(id: params[:id]
+      @chop.problem = params[:chop][:problem]
+      @chop.solution = params[:chop][:solution]
+      if @chop.save
+        redirect_to "/chops/#{@chop.id}" #not sure if correct
+      end
+      )
+    end
 end
