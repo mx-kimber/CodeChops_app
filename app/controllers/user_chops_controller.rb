d29fc26ebe732d4f9d85ed583374c3866804b6df
Kimber Ogden <mx.kimber@gmail.com>
2023-05-04 18:32:46 -0500

    Category Edit, add link to edit on show, add to index for user_chops

--- app/controllers/user_chops_controller.rb
+++ app/controllers/user_chops_controller.rb
@@ -2,6 +2,10 @@ class UserChopsController < ApplicationController
   before_action :authenticate_user
 
   def index
+
+    @user = current_user
+    @user_chops = @user.user_chops.includes(:chop)
+
     @chops = Chop.all
     if params[:category_id].present?
       @chops = Chop.where(category_id: params[:category_id])
@@ -17,7 +21,7 @@ class UserChopsController < ApplicationController
     @chop.update(rating: params[:rating])
     redirect_to chop_path(@chop)
   end
-
+#come back to this, not sure if correct
   def store
     @chop = Chop.find(params[:id])
     @chop.update(difficulty_level: params[:difficulty_level], stored_by: current_user.id)
